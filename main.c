61dccbf671bc98347ca592a1041c52812da60aa7
Harry Hsu <qspiaryb@gmail.com>
2022-06-04 12:58:30 +0800

    Add reset & reference

--- main.c
+++ main.c
@@ -3,25 +3,53 @@
 
 void Nop() {}
 
-static long dStartTimer = 0;
+static long StartTimeInMs = 0;
 
-struct FSM {
+typedef struct FSM {
     void* label;
     void* end;
+		int timer[20];
+		int timerIndex;
+		bool init;
 } FSM;
 
-#define _INNER_FSM_BEGIN(body, count) {\
-    static struct FSM fsm;\
-    fsm.end = &&FSM_END_BLOCK_##count;\
+static int ii = 0;
+
+#define FSM_RESET_PROGRESS(fsmp) fsmp->label=0;
+#define FSM_RESET_TIMER(fsmp) for(ii=0; ii<20;ii++)fsmp->timer[ii]=-1;
+#define FSM_RESET(fsmp) FSM_RESET_PROGRESS(fsmp) FSM_RESET_TIMER(fsmp)
+
+#define _INNER_FSM_BEGIN(body, c) {\
+    static FSM fsm;\
+		if (!fsm.init){fsm.init=true;FSM_RESET_TIMER((&fsm));}\
+    fsm.end = &&FSM_END_BLOCK_##c;\
     if (fsm.label) goto *fsm.label;\
     body\
-        fsm.label = 0;\
-    FSM_END_BLOCK_##count:\
+    fsm.label = 0;\
+    FSM_END_BLOCK_##c:\
         Nop();\
 }
 
-#define _AUX_FSM_BEGIN(body, count) _INNER_FSM_BEGIN(body, count)
-#define FSM_BEGIN(body) _AUX_FSM_BEGIN(body, __COUNTER__)
+#define _AUX_FSM_BEGIN(body, c) _INNER_FSM_BEGIN(body, c)
+#define __AUX_FSM_BEGIN(body) _AUX_FSM_BEGIN(body, __COUNTER__)
+
+#define _INNER_FSM_BEGIN_EXT(fsmp, body, c) {\
+    static FSM fsm;\
+		if (!fsm.init){fsm.init=true;FSM_RESET_TIMER((&fsm));}\
+		fsmp = &fsm;\
+    fsm.end = &&FSM_END_BLOCK_##c;\
+    if (fsm.label) goto *fsm.label;\
+    body\
+    fsm.label = 0;\
+    FSM_END_BLOCK_##c:\
+        Nop();\
+}
+
+#define _AUX_FSM_BEGIN_EXT(fsmp, body, c) _INNER_FSM_BEGIN_EXT(fsmp, body, c)
+#define __AUX_FSM_BEGIN_EXT(fsmp, body) _AUX_FSM_BEGIN_EXT(fsmp, body, __COUNTER__)
+
+#define FSM_GET_MACRO(_1,_2,NAME,...) NAME
+#define FSM_BEGIN(...) FSM_GET_MACRO(__VA_ARGS__, __AUX_FSM_BEGIN_EXT, __AUX_FSM_BEGIN)(__VA_ARGS__)
 
 #define _FSM_ASYNC_BLOCK(cond, c)\
     FSM_ROUTINE_##c:\
@@ -36,9 +64,10 @@ struct FSM {
     FSM_ROUTINE_##c:\
         Nop();\
     fsm.label = && FSM_ROUTINE_##c;\
-    static int cur_##c = -1;\
-    if (cur_##c == -1) cur_##c = dStartTimer;\
-    if (dStartTimer - cur_##c >= ms) cur_##c = -1;\
+		static int timer_index_##c = -1;\
+    if (timer_index_##c == -1) timer_index_##c = fsm.timerIndex++;\
+		if (fsm.timer[timer_index_##c] == -1) fsm.timer[timer_index_##c] = StartTimeInMs;\
+    if (StartTimeInMs - fsm.timer[timer_index_##c] >= ms) fsm.timer[timer_index_##c] = -1;\
     else goto *fsm.end;\
 
 #define _AUX_FSM_DELAY_MS(ms, c) _INNER_FSM_DELAY_MS(ms, c)
@@ -62,11 +91,28 @@ void test(){
     })
 }
 
+FSM* fsmp;
+
+void test2() {
+    if (flag) {
+	    flag = false;
+      FSM_RESET_PROGRESS(fsmp);
+		}
+    FSM_BEGIN(fsmp, {
+        FSM_DELAY_MS(2)
+        printf("Flag 1\n");
+				flag=true;
+        FSM_DELAY_MS(2)
+        printf("Flag 2\n");
+    })
+    
+}
+
 void main(){
   while(1) {
-      dStartTimer+=1;
-      printf("Time: %ld\n", dStartTimer);
-      test();
+      StartTimeInMs+=1;
+      printf("Time: %ld\n", StartTimeInMs);
+      test2();
       sleep(1);
   }
 }
